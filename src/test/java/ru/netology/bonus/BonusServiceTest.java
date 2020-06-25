d5e0c594ce5a0e0e09d5fd2246bd3b55b1e1e153
Kristina <kris2015.lebedeva@yandex.ru>
2020-06-25 13:05:51 +0300

    data added in data.csv

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -1,25 +1,14 @@
 package ru.netology.bonus;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
     @ParameterizedTest(name = "[{index}] {3}")
-    @CsvSource(
-            value = {
-                    "'registered user, under limit', 100060, 30, true",
-                    "'registered user, over limit', 100000060, 500, true",
-                    "'unregistered user, under limit', 100060, 10, false",
-                    "'unregistered user, over limit', 100000060, 500, false",
-                    "'registered user, under limit', 500000, 150, true",
-                    "'registered user, over limit', 300000000, 500, true",
-                    "'unregistered user, under limit', 500000, 50, false",
-                    "'unregistered user, over limit', 500000000, 500, false"
-            }
-    )
+    @CsvFileSource(resources = "/data.csv")
     void shouldCalculate(String test, long amount, long expected, boolean registered) {
         BonusService service = new BonusService();
 
